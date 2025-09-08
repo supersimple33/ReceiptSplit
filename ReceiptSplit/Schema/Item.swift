c2489f908adb8f04728437798735fc99e4f812e2
supersimple33 <40609224+supersimple33@users.noreply.github.com>
2025-09-08 09:43:59 -0400

    Adding orderer property and check property to item

--- ReceiptSplit/Schema/Item.swift
+++ ReceiptSplit/Schema/Item.swift
@@ -13,10 +13,18 @@ final class Item {
     var createdAt: Date = Date()
     var name: String
     var price: Int // cents
-	
-    init(name: String, price: Int) {
+    var orderer: Participant?
+    var check: Check
+
+    init(name: String, price: Int, forCheck check: Check) {
         self.name = name
         self.price = price
+        self.orderer = nil
+        self.check = check
+    }
+
+    func setOrderer(_ orderer: Participant) {
+        self.orderer = orderer
     }
 }
 	
