61979ac857e815da2ff0251d764ef5a3d97fd11e
supersimple33 <40609224+supersimple33@users.noreply.github.com>
2025-09-09 18:08:24 -0400

    Adding tabler stack to homescreen

--- ReceiptSplit/Screens/HomeScreen.swift
+++ ReceiptSplit/Screens/HomeScreen.swift
@@ -7,19 +7,42 @@
 
 import SwiftUI
 import SwiftData
+import Tabler
+import MaterialUIKit
 
 struct HomeScreen: View {
     @Environment(\.modelContext) private var modelContext
-    @Query var checks: [Check]
+    @Query private var checks: [Check]
+
+    private typealias Context = TablerContext<Check>
+    private typealias Sort = TablerSort<Check>
+
+    private var gridItems: [GridItem] {
+        [GridItem(.flexible(), alignment: .leading), GridItem(.flexible(), alignment: .trailing)]
+
+    }
+
+    private func row(check: Check) -> some View {
+        LazyVGrid(columns: gridItems) {
+            Text(check.name)
+            Text(check.createdAt.description).multilineTextAlignment(.trailing)
+        }
+    }
+
+    private func header(ctx: Binding<Context>) -> some View {
+        LazyVGrid(columns: gridItems) {
+            Text("Name")
+            Text("Date")
+        }
+    }
+
+    private var tablerConfig: TablerStackConfig<Check> {
+        TablerStackConfig<Check>()
+    }
 
     var body: some View {
-        VStack {
-            Text("Hello, world!")
-            Table(checks) {
-                TableColumn("Name", value: \.name)
-                TableColumn("Date", value: \.name)
-            }.tableColumnHeaders(.visible)
-            Text("Hello, world!")
+        NavigationContainer {
+            TablerStack(tablerConfig, header: header, row: row, results: checks)
         }
     }
 }
