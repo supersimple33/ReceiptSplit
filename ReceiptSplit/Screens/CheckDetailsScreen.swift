644b0f55892b318b01d37b67942568c7c79fccd9
supersimple33 <40609224+supersimple33@users.noreply.github.com>
2025-09-11 10:27:26 -0400

    Adding a base check details screen

---
+++ ReceiptSplit/Screens/CheckDetailsScreen.swift
@@ -0,0 +1,32 @@
+//
+//  CheckDetailsScreen.swift
+//  ReceiptSplit
+//
+//  Created by Addison Hanrattie on 9/11/25.
+//
+
+import SwiftUI
+import SwiftData
+
+struct CheckDetailsScreen: View {
+    var check: Check
+
+    var body: some View {
+        Text(check.name)
+    }
+}
+
+#Preview {
+    // Build a context from the preview container
+    let container = DataController.previewContainer
+    let context = container.mainContext
+
+    // Try to fetch a single Check
+    var descriptor = FetchDescriptor<Check>()
+    descriptor.fetchLimit = 1
+    descriptor.sortBy = [SortDescriptor(\Check.name, order: .forward)]
+    var fetchedCheck = try! context.fetch(descriptor).first
+
+    return CheckDetailsScreen(check: fetchedCheck!)
+        .modelContainer(container)
+}
