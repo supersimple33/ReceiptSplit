999f804fc1f993d5d6152a8e7f10d39dd96cf534
supersimple33 <40609224+supersimple33@users.noreply.github.com>
2025-09-12 10:32:54 -0400

    Adding photopicker logic

--- ReceiptSplit/Screens/CaptureScreen.swift
+++ ReceiptSplit/Screens/CaptureScreen.swift
@@ -6,15 +6,37 @@
 //
 
 import SwiftUI
+import PhotosUI
 import MaterialUIKit
 
 struct CaptureScreen: View {
-    
+    @State private var selectedItem: PhotosPickerItem?
+    @State private var image: Image?
 
     var body: some View {
         NavigationContainer {
-            Text("Hello, World!")
-                .navigationContainerTopBar(title: "Capture", backButtonHidden: false, style: .inline)
+            HStack {
+                PhotosPicker(
+                    selection: $selectedItem,
+                    matching: .images) {
+                        IconButton(systemImage: "photo", style: .filled) {
+                            Void()
+                        }.allowsHitTesting(false)
+                    }
+                if let image {
+                    image
+                        .resizable()
+                        .frame(width: 200, height: 200)
+                }
+            }
+        }.onChange(of: selectedItem) { prevItem, newItem in
+            Task {
+                if let data = try? await newItem?.loadTransferable(type: Data.self) {
+                    if let uiImage = UIImage(data: data) {
+                        image = Image(uiImage: uiImage)
+                    }
+                }
+            }
         }
     }
 }
