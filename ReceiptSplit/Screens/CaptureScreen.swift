d0eed2d598ef9b20a4e83a41c2505f33bda6cc6f
supersimple33 <40609224+supersimple33@users.noreply.github.com>
2025-09-21 12:20:11 -0400

    Making the capture screen filling

--- ReceiptSplit/Screens/CaptureScreen.swift
+++ ReceiptSplit/Screens/CaptureScreen.swift
@@ -13,56 +13,36 @@ import MijickCamera
 struct CaptureScreen: View {
     @Environment(Router.self) private var router
     @State private var selectedItem: PhotosPickerItem?
-    @State private var image: Image?
 
     // TODO: add error snackbar
 
     var body: some View {
-        Container {
-            VStack(spacing: 16) {
-                MCamera()
-                    .setAudioAvailability(false)
-                    .setCameraOutputType(.photo)
-                    .setResolution(.photo)
-                    .setCameraHDRMode(.on)
-                    .setFlashMode(.auto)
-                    .onImageCaptured { image, controller in
-                        if let ciImage = CIImage(image: image) {
-                            router.navigateTo(route: .analysis(image: ciImage))
-                        } else {
-                            // TODO: error handling
-                        }
-                    }
-                    .setCameraScreen({ cameraManager, namespace, closeMCameraAction in
-                        DefaultCameraScreen(
-                            cameraManager: cameraManager,
-                            namespace: namespace,
-                            closeMCameraAction: closeMCameraAction,
-                        )
-                        .cameraOutputSwitchAllowed(false)
-                        .closeButtonAllowed(false)
-                    })
-                    .startSession()
-//                HStack {
-//                    PhotosPicker(
-//                        selection: $selectedItem,
-//                        matching: .images
-//                    ) {
-//                        IconButton(systemImage: "photo", style: .filled) {
-//                            Void()
-//                        }.allowsHitTesting(false)
-//                    }
-//
-////                    ActionButton("Capture", style: .filledStretched) {
-////                        model.camera.takePhoto()
-////                    }
-////
-////                    IconButton(systemImage: "arrow.trianglehead.2.clockwise.rotate.90.camera.fill", style: .filled) {
-////                        model.camera.switchCaptureDevice()
-////                    }
-//                }
+        MCamera()
+            .setAudioAvailability(false)
+            .setCameraOutputType(.photo)
+            .setResolution(.photo)
+            .setCameraHDRMode(.on)
+            .setFlashMode(.auto)
+            .onImageCaptured { image, controller in
+                if let ciImage = CIImage(image: image) {
+                    router.navigateTo(route: .analysis(image: ciImage))
+                } else {
+                    // TODO: error handling
+                }
             }
-        }
+            .setCameraScreen({ cameraManager, namespace, closeMCameraAction in
+                DefaultCameraScreen(
+                    cameraManager: cameraManager,
+                    namespace: namespace,
+                    closeMCameraAction: closeMCameraAction,
+                )
+                .cameraOutputSwitchAllowed(false)
+            })
+            .setCloseMCameraAction {
+                router.navigationPath.removeLast()
+            }
+            .startSession()
+        .navigationBarBackButtonHidden(true)
         .onChange(of: selectedItem) { _, newItem in
             Task {
                 guard let newItem else { return }
